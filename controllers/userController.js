c3639db94c8d7e2ad739d46fce4eb1f7eb615480
Ta Hong Anh <Ta Anh>
2020-11-07 14:43:29 +0700

    firstcommit

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+
+
+exports.getAllUsers = (req,res)=>{
+    res.status(500).json({
+        status:'error',
+        massage: 'This route not define!'
+    });
+};
+
+exports.getUser = (req,res)=>{
+    res.status(500).json({
+        status:'error',
+        massage: 'This route not define!'
+    });
+};
+
+exports.createUser = (req,res)=>{
+    res.status(500).json({
+        status:'error',
+        massage: 'This route not define!'
+    });
+};
+
+exports.updateUser = (req,res)=>{
+    res.status(500).json({
+        status:'error',
+        massage: 'This route not define!'
+    });
+};
+
+exports.deleteUser = (req,res)=>{
+    res.status(500).json({
+        status:'error',
+        massage: 'This route not define!'
+    });
+};
