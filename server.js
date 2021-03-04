b928e24ff1ae76098fbea3c2a67313aa17928fa8
Ta Hong Anh <Ta Anh>
2021-03-04 08:44:21 +0700

    import develop data

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const app = require('./app');
 const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
 
 mongoose
-    // .connect(process.env.DATABASE_LOCAL, {
     .connect(DB,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
